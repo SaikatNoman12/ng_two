8292bbe4ee0107bddc6c64044ee4d3ab11284242
saikatnoman12 <noommaa240@gmail.com>
2022-11-15 18:20:44 +0600

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoute: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'contacts', component: ContactsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ContactsComponent,
+    ProductsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoute),
   ],
   providers: [],
   bootstrap: [AppComponent]
