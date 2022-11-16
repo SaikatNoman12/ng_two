8b7b10d8813cf33bf8aef9352ff86538f8fc2fc6
saikatnoman12 <noommaa240@gmail.com>
2022-11-16 07:49:48 +0600

    add child routing routing

---
+++ src/app/products/bird/bird.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bird',
+  templateUrl: './bird.component.html',
+  styleUrls: ['./bird.component.css']
+})
+export class BirdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -12,4 +12,44 @@ export class ProductsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  // 
+
+  // 
+
+  // https://media.4rgos.it/i/Argos/0821-m0014-m007-m050-asym-m008-m022-washing-machine-as270753549?maxW=768&qlt=75&fmt.jpeg.interlaced=true
+
+  // 
+
+  allData:any[] = [
+    {
+      name:'Bird',
+      info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rem nisi libero quae dicta! Lorem ipsum dolor sit amet.',
+      btnData:'Bird',
+      routerLin:'bird',
+      imgIn:'https://images.unsplash.com/photo-1444464666168-49d633b86797?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmlyZHxlbnwwfHwwfHw%3D&w=1000&q=80'
+    },
+    {
+      name:'Laptop',
+      info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rem nisi libero quae dicta! Lorem ipsum dolor sit amet.',
+      btnData:'Laptop',
+      routerLin:'laptop',
+      imgIn:'https://detikapp.com/wp-content/uploads/2022/01/Review-10-Rekomendasi-Merk-Laptop-Terbaik-Harganya-Terbaru-2022.jpg'
+    },
+    {
+      name:'Mobile',
+      info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rem nisi libero quae dicta! Lorem ipsum dolor sit amet.',
+      btnData:'Mobile',
+      routerLin:'mobile',
+      imgIn:'https://www.tecno-mobile.in/storage/images/20220908/a809a228c03832225f2a048f3d4307b5.jpg'
+    },
+    {
+      name:'Washing Machine',
+      info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rem nisi libero quae dicta! Lorem ipsum dolor sit amet.',
+      btnData:'Washing Machine',
+      routerLin:'washing-machine',
+      imgIn:'https://www.lg.com/uk/images/microsite/washing-machine-buying-guide/LG-Washing-Machine-Buying-Guide-02-M.jpg'
+    },
+  ]
+
 }
