a35e0d08b81299dd37384049081a98a3fd4008ef
saikatnoman12 <noommaa240@gmail.com>
2022-11-28 01:33:43 +0600

    add template driven form

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -8,15 +8,65 @@ import { NgForm } from '@angular/forms';
 })
 export class TemplateDrivenFormComponent implements OnInit {
 
-  @ViewChild('refer') myForm!:NgForm;
+  @ViewChild('refer') myForm!: NgForm;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onSubmitForm(refer: NgForm) {
-    console.log(this.myForm);
+  genders: any[] = [
+    { id: '1', value: 'Male' },
+    { id: '2', value: 'Female' },
+    { id: '3', value: 'Both' }
+  ];
+
+  mySelect: string = 'Angular';
+
+  showData: string = '';
+  showDefaultData: string = 'Karim Khan';
+  defaultGender: string = 'Male';
+
+  showFormData: boolean = false;
+
+  onFormData: any = {
+    uname: '',
+    email: '',
+    course: '',
+    gender: ''
   }
+
+  onSubmitForm(refer: NgForm) {
+    this.onFormData.uname = this.myForm.value.userDetails.username;
+    this.onFormData.email = this.myForm.value.userDetails.email;
+    this.onFormData.course = this.myForm.value.course;
+    this.onFormData.gender = this.myForm.value.gander;
+    this.showFormData = true;
+  }
+
+  onUpdate() {
+    this.myForm.setValue({
+      userDetails: {
+        username: 'Noman',
+        email: ''
+      },
+      course: '',
+      gander: ''
+    });
+    
+    this.showFormData = false;
+  }
+
+  updateUserName() {
+    this.myForm.form.patchValue({
+      userDetails: {
+        username: 'Rahim Khana'
+      }
+    });
+
+    this.showFormData = false;
+  }
+
+
 
 }
